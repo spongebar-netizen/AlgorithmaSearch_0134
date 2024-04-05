43c4142479a13945a1acef872c1f64884091bfef
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-05 10:47:16 +0700

    for (i = 0; i < n; i++)

--- AlgorithmaSearch_0134/AlgorithmaSearch_0134.cpp
+++ AlgorithmaSearch_0134/AlgorithmaSearch_0134.cpp
@@ -15,3 +15,7 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+	cout << "\n----------------------\n";
+	cout << "Enter Element Array\n";
+	cout << "------------------------\n";
+	for (i = 0; i < n; i++)
