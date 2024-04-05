36f1602a4278863990eab4d5efa56946fc34a484
spongebar-netizen <fatanjrz1@gmail.com>
2024-04-05 10:46:00 +0700

    masukkan nomer elemen

--- AlgorithmaSearch_0134/AlgorithmaSearch_0134.cpp
+++ AlgorithmaSearch_0134/AlgorithmaSearch_0134.cpp
@@ -4,3 +4,14 @@ using namespace std;
 int arr[20];
 int n;
 int i;
+void input()
+{
+	while (true)
+	{
+		cout << "Enter The number of element in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
